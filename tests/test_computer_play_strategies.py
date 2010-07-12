a367928e5e509cd4c96b4f3b6fffd7036e43015f
Tomasz Nazar <tomasz.nazar@gmail.com>
2010-07-12 15:34:45 +0200

    .

--- tests/test_computer_play_strategies.py
+++ tests/test_computer_play_strategies.py
@@ -50,4 +50,37 @@ class Test(BaseTest):
 
         self.assertEqual(4, len(game.players))
         
+    def test_01_predefind_game(self):
+        game = Game()
+
+        game.add_player(
+            ComputerPlayer('P-Engine', 
+                OUTPOST, 
+                strategy=PredefinedStrategy(
+                    predefined_moves=[
+                        [Move(HqPawn(), 'A1', 'A')],
+                        [Move(RunnerPawn(), 'A2', 'B'), KeepInHand(BattlePawn())],
+                        [Move(RunnerPawn(), 'B1', 'C'), Discard(RunnerPawn())],
+                        [Battle(BattlePawn()), Discard(RunnerPawn()), Discard(RunnerPawn())]
+                    ]
+                )
+            )
+        )
         
+        game.add_player(
+            HumanPlayer('Tomasz', 
+                OUTPOST,
+                strategy=PredefinedStrategy(
+                    predefined_moves=[
+                        [Move(HqPawn(), 'E1', 'F')],
+                        [Move(RunnerPawn(), 'E2', 'B'), Discard(BattlePawn())],
+                        [Move(RunnerPawn(), 'E3', 'C'), Discard(RunnerPawn())]
+                    ]
+                )
+            )
+        )
+        
+        game.player_turn(game.computer())
+        
+#        self.assertTrue(game.board.pawn('A1'))
+#        self.assertEquals('A', game.board.pawn('A1').direction)
